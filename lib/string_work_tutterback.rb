3351b69904ef6f5cf308991ce42df7a156685a17
Todd Utterback <todd.j.utterback@gmail.com>
2016-04-12 10:15:39 -0600

    init commit

--- lib/string_work_tutterback.rb
+++ lib/string_work_tutterback.rb
@@ -1,5 +1,24 @@
 require "string_work_tutterback/version"
 
 module StringWorkTutterback
-  # Your code goes here...
+  def self.reversify(str)
+    str.split('').reverse.join('')
+  end
+
+  def self.casify(str)
+    to_case= [:upcase, :downcase]
+    arr = str.split("")
+    arr.each_with_index do |letter, i|
+      this_case = to_case[rand(2)]
+      arr[i] = letter.send(this_case)
+    end
+  end
+
+  def self.spacify(str, spaces = 0)
+    new_string = str
+    spaces.times do
+      new_string = new_string.split("").join(" ")
+    end
+    new_string
+  end
 end
--- spec/string_work_tutterback_spec.rb
+++ spec/string_work_tutterback_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe StringWorkTutterback do
+  subject { StringWorkTutterback}
+
+describe StringWorkTutterback do
   it 'has a version number' do
     expect(StringWorkTutterback::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y   S t r i n g')
+    end
   end
 end
